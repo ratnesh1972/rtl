3c6c746f6477c0c21c55dc238844fba39c2b4efa
Ratnesh Patil <ratneshdesigns@gmail.com>
2025-07-26 11:47:34 +0530

    tests: setup rtl with vitest

--- src/componenets/Welcome.test.jsx
+++ src/componenets/Welcome.test.jsx
@@ -1,3 +1,13 @@
-test('renders welcome component with correct greeting', () => {
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react';
+import Welcome from './Welcome';
+
+test('true is true', () => {
     expect(true).toBe(true)
+})
+
+test('welcome is true', () => {
+    render(<Welcome name="Ratnesh" />)
+    const greeting = screen.getByText('Hello, Ratnesh!')
+    expect(greeting).toBeInTheDocument()
 })
